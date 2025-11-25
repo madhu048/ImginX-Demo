219221d0133bad76b09db9f3c6569482587262cc
Madhu <133554430+madhu048@users.noreply.github.com>
2025-11-25 20:40:53 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -33,6 +33,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    screenshot: 'on', 
+    video: 'on', 
   },
   timeout:2000000,
 
